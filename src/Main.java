def66bf05668b640c723833d1bba22869db47757
schmidtronaldo <schmidt_dev_2024@outlook.com>
2025-02-13 15:27:29 -0300

    reorganizando código...

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,26 @@
+import java.util.Scanner;
 
 public class Main {
 
+	
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
-		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
-
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
-		
-		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
+            try (Scanner scanner = new Scanner(System.in)) {
+                Cliente userClient = new Cliente();
+                
+                System.out.print("Digite o nome do cliente: ");
+                String nome = scanner.nextLine();
+                userClient.setNome(nome);
+                
+                
+                Conta cc = new ContaCorrente(userClient);
+                Conta poupanca = new ContaPoupanca(userClient);
+                
+                cc.depositar(100);
+                cc.transferir(100, poupanca);
+                
+                cc.imprimirExtrato();
+                poupanca.imprimirExtrato();
+            }
 	}
 
 }
